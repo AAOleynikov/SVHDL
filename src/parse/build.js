cf3660fc51ce3a4060b20a37e2eadad1436c6be9
zhugeo <zhugeo@mail.ru>
2025-08-29 18:42:00 +0300

    code style changes

--- src/parse/build.js
+++ src/parse/build.js
@@ -1,8 +1,8 @@
 /* Этот файл не включается в бандл. Этот скрипт нужен только для того, чтобы построить парсер из g4-файлов */
 import * as fs from "node:fs";
 import { exec } from "child_process";
-import { fileURLToPath } from 'node:url';
-import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 
 const DIRECTORY_FOR_CODEGEN = "./build";
 
@@ -12,7 +12,6 @@ const PARSERS_DIRECTORY_PATH = resolve(__dirname);
 
 process.chdir(PARSERS_DIRECTORY_PATH);
 
-
 if (!fs.existsSync(DIRECTORY_FOR_CODEGEN)) {
   fs.mkdirSync(DIRECTORY_FOR_CODEGEN);
   console.log('Folder "build" created');
@@ -20,8 +19,7 @@ if (!fs.existsSync(DIRECTORY_FOR_CODEGEN)) {
   console.log('Folder "build" already exists');
 }
 
-const command =
-  `antlr4 -Dlanguage=JavaScript vhdlLexer.g4 vhdlParser.g4 -visitor -listener -o ${DIRECTORY_FOR_CODEGEN}`;
+const command = `antlr4 -Dlanguage=JavaScript vhdlLexer.g4 vhdlParser.g4 -visitor -listener -o ${DIRECTORY_FOR_CODEGEN}`;
 exec(command, (error, _, stderr) => {
   if (error) {
     console.error(`Error executing command: ${stderr}`);
