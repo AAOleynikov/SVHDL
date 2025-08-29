86e459bf5c51842485ab7bbb0f1bfba28d3e6fd0
zhugeo <zhugeo@mail.ru>
2025-08-29 14:14:35 +0300

    refactor build parsers script

--- src/parse/build.js
+++ src/parse/build.js
@@ -1,28 +1,31 @@
 /* Этот файл не включается в бандл. Этот скрипт нужен только для того, чтобы построить парсер из g4-файлов */
 import * as fs from "node:fs";
 import { exec } from "child_process";
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
 
-process.chdir(
-  import.meta.url.slice(8, import.meta.url.length - "/build.js".length)
-); // Чтобы запускалось ровно там, где находится этот скрипт
+const DIRECTORY_FOR_CODEGEN = "./build";
 
-const folderPath = "./build";
-const command =
-  "antlr4 -Dlanguage=JavaScript ../vhdlLexer.g4 ../vhdlParser.g4 -visitor -listener";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const PARSERS_DIRECTORY_PATH = resolve(__dirname);
 
-if (!fs.existsSync(folderPath)) {
-  fs.mkdirSync(folderPath);
-  console.log('Folder "build" creating error: success');
+process.chdir(PARSERS_DIRECTORY_PATH);
+
+
+if (!fs.existsSync(DIRECTORY_FOR_CODEGEN)) {
+  fs.mkdirSync(DIRECTORY_FOR_CODEGEN);
+  console.log('Folder "build" created');
 } else {
   console.log('Folder "build" already exists');
 }
 
-process.chdir(folderPath);
-
-exec(command, (error, stdout, stderr) => {
+const command =
+  `antlr4 -Dlanguage=JavaScript vhdlLexer.g4 vhdlParser.g4 -visitor -listener -o ${DIRECTORY_FOR_CODEGEN}`;
+exec(command, (error, _, stderr) => {
   if (error) {
     console.error(`Error executing command: ${stderr}`);
   } else {
-    console.log(`Unerror executing command: success code 404`);
+    console.log(`Succesfully generated parsers`);
   }
 });
